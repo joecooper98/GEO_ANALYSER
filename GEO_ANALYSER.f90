83b1527f4f40433eac3a8f6cc043871f2e46cf29
joecooper98 <josephcharlescooper@gmail.com>
2022-04-25 10:13:47 +0100

    change to switch case statement in decider

--- GEO_ANALYSER.f90
+++ GEO_ANALYSER.f90
@@ -1,17 +1,13 @@
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!                                               !
-! RMSD CACULATION SCRIPT                        !
-! Joe Cooper 2022                               !
-!                                               !
-! MUST BE COMPILED WITH LAPACK LIBRARY, e.g.    !
-! $ gfortran RMSD.f90 -llapack -o RMSD.o        !
-!                                               !
-! USE KABSCH ALGORITHM WITH SVD                 !
-! GEOMETRIES ARE .xyz FILES PASSED THROUGH COM- !
-! MAND LINE, e.g.                               !
-! $ ./RMSD.o geom1.xyz geom2.xyz                !
-!                                               !
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Geometry analyser script                        !
+! Written by Joe Cooper, 2022                     !
+! Takes in .xyz files and performs analyses       !
+! Compile with something like                     !
+! $ gfortran -lblas -llapack -O3 GEO_ANALYSER.f90 !
+! Run with something like                         !
+! $ ./GEO_ANALYSER input traj.xyz ref.xyz         !
+! Feel free to change to your liking              !
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 program geo_analyser
   implicit none
 
@@ -47,23 +43,34 @@ program geo_analyser
     CALL read_xyz(GEOM2, file2, f2, atoms2,io2,time)
   end if 
 
+  ! read the input file - will return a array of characters deciding the operations
   call read_input(inpfile, finp, str, no_operations)
+
+  ! this will be where the values for each of the operations is held
   allocate(val(no_operations))
 
 
-  do 
+  do  ! keep looping until end of file is hit
+  ! this will loop over every geometry in a trajectory
 
+  ! read the geometry for this time step
+  ! overwrites the geometry from the last time step
   CALL read_xyz(GEOM1, file1, f1, atoms, io1,time)
 
   if (io1 .LT. 0) then
-    exit
+          exit ! will exit if the reader hits the end of the file
   end if 
 
-  do i = 1,no_operations
+  do i = 1,no_operations ! loop over all of the operations
+  ! call the main flow controller - this will read the string, work out what to do, and then do it
+  ! TO DO improve this by reading through once, and then assigning an integer array to help
   call decider(trim(str(i)), GEOM1, GEOM2, val(i),atoms)
   end do
+  ! writes the values to the main out - then pipe to file.
   call printer(val, no_operations)
   end do
+
+  ! deallocate everything at the end.
   if (allocated(GEOM2)) then
           deallocate(GEOM2)
           deallocate(atoms2)
@@ -80,6 +87,38 @@ program geo_analyser
   end if
 contains
 
+  subroutine decider(str, GEOM1, GEOM2, val, atoms)
+    CHARACTER(LEN=*), intent(IN) ::  str
+    DOUBLE PRECISION, intent(IN) :: GEOM1(:,:), GEOM2(:,:)
+    DOUBLE PRECISION, intent(OUT) :: val
+    character(len=2), intent(in) :: atoms(:)
+    character(len=5) :: parse
+    integer, allocatable :: indices(:)
+
+
+    call string_split(str,parse, indices)
+
+    select case (StrUpCase(trim(parse)))
+
+    case ('RMSD')
+      call RMSD(GEOM1, GEOM2, val)
+    case ('RNOH')
+      call RNOH(GEOM1, GEOM2, val,atoms)
+    case ('DIST')
+      val = DIST(GEOM1,indices)
+    case ('ANGL')
+      val = ANG(GEOM1,indices)
+    case ('DIHE')
+      val = DIHE(GEOM1,indices)
+    case default
+      write(*,*) 'Not implemented yet!'
+      stop
+    end select
+    if (allocated(indices)) then
+    deallocate(indices)
+    end if
+  end subroutine
+
 subroutine printer(x,n)
                 double precision, intent(in) :: x(:)
                 integer,intent(in) :: n
@@ -93,96 +132,6 @@ subroutine printer(x,n)
                 end if 
                 end do
                 end subroutine
-
-
-  subroutine read_input(filename, fid, str, no_operations)
-    CHARACTER(LEN=*), intent(IN) :: filename
-    CHARACTER(LEN=50), dimension(:), allocatable, intent(out) :: str
-    integer, intent(in) :: fid
-    integer, intent(out) :: no_operations
-    integer :: io,  i
-
-    no_operations = 0 
-    open(fid, file=trim(filename))
-    do
-    read(fid, *, iostat=io)
-    if (io .lt. 0) then
-      exit
-    end if
-    no_operations = no_operations + 1
-    end do 
-
-    allocate(str(no_operations))
-    rewind(fid)
-    do i = 1,no_operations
-    read(fid,'(A)') str(i)
-    end do 
-
-  end subroutine
-
-  subroutine decider(str, GEOM1, GEOM2, val, atoms)
-    CHARACTER(LEN=*), intent(IN) ::  str
-    DOUBLE PRECISION, intent(IN) :: GEOM1(:,:), GEOM2(:,:)
-    DOUBLE PRECISION, intent(OUT) :: val
-    character(len=2), intent(in) :: atoms(:)
-    character(len=5) :: parse
-    integer, allocatable :: indices(:)
-
-
-    call string_split(str,parse, indices)
-
-    if (StrUpCase(trim(parse)) .eq. 'RMSD') then
-      call RMSD(GEOM1, GEOM2, val)
-    elseif (StrUpCase(trim(parse)) .eq. 'RNOH') then
-      call RNOH(GEOM1, GEOM2, val,atoms)
-    else if (StrUpCase(trim(parse)) .eq. 'DIST') then
-      val = DIST(GEOM1,indices)
-    else if (StrUpCase(trim(parse)) .eq. 'ANGL') then
-      val = ANG(GEOM1,indices)
-    else if (StrUpCase(trim(parse)) .eq. 'DIHE') then
-      val = DIHE(GEOM1,indices)
-    else 
-      write(*,*) 'Not implemented yet!'
-      stop
-    end if 
-    if (allocated(indices)) then
-    deallocate(indices)
-    end if
-  end subroutine
-
-  subroutine string_split(str, output, indices)
-    character(len=*), intent(IN) :: str
-    character(len=5), intent(out) :: output
-    integer, allocatable :: indices(:)
-    integer :: noinputs
-    character(len=45) :: intstring
-
-    read(str,'(A5,A)') output, intstring
-
-    noinputs = count_spaces(trim(intstring))
-    if (noinputs .ne. 0) then
-      allocate(indices(noinputs+1))
-      read(intstring,*) indices(:)
-    else
-            allocate(indices(1))
-!otherwise it gets mad
-    end if 
-
-  end subroutine 
-
-  integer function count_spaces(str) result(n)
-          character(len=*):: str
-          integer :: i
-
-          n=0
-          do i = 1,len_trim(str)
-            if (str(i:i) .eq. ' ') then
-                    n = n+1
-            end if
-            end do
-            end function
-
-
 
   subroutine RMSD(GEOM1, GEOM2, val)
     DOUBLE PRECISION, intent(IN) :: GEOM1(:,:), GEOM2(:,:)
@@ -293,6 +242,67 @@ subroutine printer(x,n)
 
 
   end function
+
+  subroutine read_input(filename, fid, str, no_operations)
+    CHARACTER(LEN=*), intent(IN) :: filename
+    CHARACTER(LEN=50), dimension(:), allocatable, intent(out) :: str
+    integer, intent(in) :: fid
+    integer, intent(out) :: no_operations
+    integer :: io,  i
+
+    no_operations = 0 
+    open(fid, file=trim(filename))
+    do
+    read(fid, *, iostat=io)
+    if (io .lt. 0) then
+      exit
+    end if
+    no_operations = no_operations + 1
+    end do 
+
+    allocate(str(no_operations))
+    rewind(fid)
+    do i = 1,no_operations
+    read(fid,'(A)') str(i)
+    end do 
+
+  end subroutine
+
+
+  subroutine string_split(str, output, indices)
+    character(len=*), intent(IN) :: str
+    character(len=5), intent(out) :: output
+    integer, allocatable :: indices(:)
+    integer :: noinputs
+    character(len=45) :: intstring
+
+    read(str,'(A5,A)') output, intstring
+
+    noinputs = count_spaces(trim(intstring))
+    if (noinputs .ne. 0) then
+      allocate(indices(noinputs+1))
+      read(intstring,*) indices(:)
+    else
+            allocate(indices(1))
+!otherwise it gets mad
+    end if 
+
+  end subroutine 
+
+  integer function count_spaces(str) result(n)
+          character(len=*):: str
+          integer :: i
+
+          n=0
+          do i = 1,len_trim(str)
+            if (str(i:i) .eq. ' ') then
+                    n = n+1
+            end if
+            end do
+            end function
+
+
+
 
   double precision pure function rad2deg(x) result(y)
           double precision, intent(in) :: x
